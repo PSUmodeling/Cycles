b4ba6fd93e7f051f0765d894129b429005952adf
Yuning Shi <shiyuning@gmail.com>
2015-03-17 13:39:12 -0400

    Finalize Irrigation.c

--- src/Irrigation.c
+++ src/Irrigation.c
@@ -4,6 +4,16 @@ double FindIrrigationVolume (int opLayer, double opWaterDepletion, const SoilStr
 {
     /*
      * This sub calculates the volume of an automatic irrigation
+     * -----------------------------------------------------------------------
+     * LOCAL VARIABLES
+     *
+     * Variable             Type        Description
+     * ==========           ==========  ====================
+     * i		    int		Loop counter
+     * depletionZonePAW	    double
+     * depletionZoneWater   double
+     * irrigation_vol	    double	Volume of an automatic irrigation
+     *					  [return value]
      */
     int             i;
     double          depletionZonePAW = 0.0;
@@ -20,13 +30,16 @@ double FindIrrigationVolume (int opLayer, double opWaterDepletion, const SoilStr
             depletionZonePAW += Soil->PAW[i] * Soil->layerThickness[i];
             depletionZoneWater += (Soil->waterContent[i] - Soil->PWP[i]) * Soil->layerThickness[i];
         }
-        else                    /* this adds deficit below PWP */
+        else
+	{
+	    /* This adds deficit below PWP */
             depletionZonePAW += (Soil->FC[i] - Soil->waterContent[i]) * Soil->layerThickness[i];
+	}
     }
 
     if (depletionZoneWater + Soil->irrigationVol < depletionZonePAW * (1.0 - opWaterDepletion))
     {
-        /* convert m to mm */
+        /* Convert m to mm */
         irrigation_vol = (depletionZonePAW - depletionZoneWater) * 1000.0 - Soil->irrigationVol;
     }
 
