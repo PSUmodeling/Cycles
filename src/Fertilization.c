f2c4ab88d7fd82b72b67758c32c8815ab9342105
Yuning Shi <shiyuning@gmail.com>
2015-03-17 13:25:38 -0400

    Finalize Fertilization.c

--- src/Fertilization.c
+++ src/Fertilization.c
@@ -2,10 +2,18 @@
 
 void ApplyFertilizer (FieldOperationStruct *fixedFertilization, SoilStruct *Soil, ResidueStruct *Residue)
 {
+    /*
+     * -----------------------------------------------------------------------
+     * LOCAL VARIABLES
+     *
+     * Variable             Type        Description
+     * ==========           ==========  ====================
+     * layer		    int
+     */
     int             layer;
 
-    /* incorporates fertilizer with option to put manure C and manure N in the
-     * surface, but mineral components are added to layer 1 */
+    /* Incorporates fertilizer with option to put manure C and manure N in the
+     * surface, but mineral components are added to layer 0 */
     if (fixedFertilization->opLayer > 0)
     {
         layer = fixedFertilization->opLayer - 1;
@@ -18,6 +26,7 @@ void ApplyFertilizer (FieldOperationStruct *fixedFertilization, SoilStruct *Soil
     {
         Residue->manureSurfaceC += fixedFertilization->opMass * fixedFertilization->opC_Organic;
         Residue->manureSurfaceN += fixedFertilization->opMass * fixedFertilization->opN_Organic;
+
         layer = 0;
         Soil->NO3[layer] += fixedFertilization->opMass * fixedFertilization->opN_NO3;
         Soil->NH4[layer] += fixedFertilization->opMass * fixedFertilization->opN_NH4;
