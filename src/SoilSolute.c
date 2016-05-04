03394f3aca1f2f9eeb7831d1fa45d76b3d05a74d
Yuning Shi <shiyuning@gmail.com>
2016-05-04 13:04:48 -0400

    MM-PIHM edits

--- src/SoilSolute.c
+++ src/SoilSolute.c
@@ -1,5 +1,87 @@
+#ifdef _PIHM_
+#include "pihm.h"
+#else
 #include "Cycles.h"
+#endif
 
+#ifdef _PIHM_
+void SoluteTransport (elem_struct *elem, int numele)
+{
+    int             i, j;
+
+    /* Link solute structures to soil solutes */
+    for (i = 0; i < numele; i++)
+    {
+        for (j = 0; j < elem[i].ps.nsoil; j++)
+        {
+            elem[i].NO3sol.soluteMass[j] = elem[i].soil.NO3[j];
+            elem[i].NH4sol.soluteMass[j] = elem[i].soil.NH4[j];
+        }
+    }
+
+    for (i = 0; i < numele; i++)
+    {
+        Adsorption (elem[i].ps.sldpth, elem[i].ws.sh2o, elem[i].soil.BD, elem[i].ps.nsoil, 0.0, &elem[i].NO3sol);
+
+    }
+
+    for (i = 0; i < numele; i++)
+    {
+        Adsorption (elem[i].ps.sldpth, elem[i].ws.sh2o, elem[i].soil.BD, elem[i].ps.nsoil, 5.6, &elem[i].NH4sol);
+    }
+
+//        for (i = 0; i < numele; i++)
+//        {
+//            for (k = 0; k < 3; k++)
+//            {
+//                if (elem[i].nabr[k] > 0)
+//                {
+//                    nabr = elem + elem[i].nabr[k] - 1;
+//                    for (kk = 0; kk < 3; kk++)
+//                    {
+//                        if (nabr->nabr[kk] == i - 1)
+//                        {
+//                            k_nabr = kk;
+//                            break;
+//                        }
+//                    }
+//
+//                    LatTrnsp (elem + i, nabr, solute[ks][i].soluteFluxLat[k], solute[ks][elem[i].nabr[k] - 1].soluteFluxLat[k]);
+//
+//                }
+//                else
+//                {
+//                }
+//            }
+//        }
+//    }
+}
+
+void Adsorption (const double *sldpth, const double *sh2o, const double *bd, int nsoil, double Sol_Kd, solute_struct *solute)
+{
+    int             j;
+    double          waterInitial;
+
+    for (j = 0; j < MAXLYR; j++)
+    {
+        solute->soluteConc[j] = 0.0;
+        solute->soluteMassAdsorbed[j] = 0.0;
+    }
+
+    for (j = 0; j < nsoil; j++)
+    {
+        waterInitial = sldpth[j] * WATER_DENSITY * sh2o[j];
+
+        if (solute->soluteMass[j] > 0.0)
+        {
+            solute->soluteConc[j] = LinearEquilibriumConcentration (Sol_Kd, bd[j], sldpth[j], sh2o[j], solute->soluteMass[j]);
+        }
+
+
+        solute->soluteMassAdsorbed[j] = solute->soluteMass[j] - waterInitial * solute->soluteConc[j];
+    }
+}
+#else
 void SoluteTransport (int totalLayers, double Sol_Kd, double WInConc, double *leachate, const double *WFlux, double *soluteMass, const double *BD, const double *thickness, const double *porosity, const double *WCinitial)
 {
     /*
@@ -65,6 +147,7 @@ void SoluteTransport (int totalLayers, double Sol_Kd, double WInConc, double *le
         soluteMass[i] = soluteMassAdsorbed + soluteMassSolution;
     }
 }
+#endif
 
 void SoluteTransportEvaporation (int totalLayers, double Sol_Kd, const double *WFlux, double *soluteMass, const double *BD, const double *thickness, const double *porosity, double *WCinitial)
 {
